2af500a88d006da7fadd97a9fc6b674764d90bfa
Yashas PS <53535790+Yashas-pissay@users.noreply.github.com>
2023-06-10 01:24:20 +0530

    Data Transformation Completed

--- visa/pipeline/pipeline.py
+++ visa/pipeline/pipeline.py
@@ -1,13 +1,22 @@
-import sys
-from visa.constant import *
+from collections import namedtuple
+from datetime import datetime
+import uuid
+from visa.config.configuration import Configuartion
 from visa.logger import logging
 from visa.exception import CustomException
-from visa.entity.config_entity import *
-from visa.utils.utils import read_yaml_file #helper Function
+from threading import Thread
+from typing import List
+
+from multiprocessing import Process
+from visa.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact, DataTransformationArtifact
 from visa.components.data_ingestion import DataIngestion
 from visa.components.data_validation import DataValidation
-from visa.entity.artifact_entity import DataIngestionArtifact, DataValidationArtifact
-from visa.config.configuration import Configuartion
+from visa.components.data_transformation import DataTransformation
+
+import os, sys
+from collections import namedtuple
+from datetime import datetime
+import pandas as pd
 
 
 class Pipeline():
@@ -31,7 +40,22 @@ class Pipeline():
                                              data_ingestion_artifact=data_ingestion_artifact)
             return data_validation.initiate_data_validation()
         except Exception as e:
-            raise CustomException(e, sys) from e        
+            raise CustomException(e, sys) from e    
+        
+
+    def start_data_transformation(self,
+                                  data_ingestion_artifact: DataIngestionArtifact,
+                                  data_validation_artifact: DataValidationArtifact) -> DataTransformationArtifact:
+        try:
+            data_transfromation = DataTransformation(
+                data_transformation_config=self.config.get_data_transformation_config(),
+                data_ingestion_artifact=data_ingestion_artifact,
+                data_validation_artifact=data_validation_artifact
+            )
+
+            return data_transfromation.initiate_data_transformation()
+        except Exception as e:
+            raise CustomException(e, sys) from e            
         
     def run_pipeline(self):
         try:
@@ -39,5 +63,7 @@ class Pipeline():
 
             data_ingestion_artifact = self.start_data_ingestion()
             data_validation_artifact = self.start_data_validation(data_ingestion_artifact=data_ingestion_artifact)
+            data_transfromation_artifact = self.start_data_transformation(data_ingestion_artifact=data_ingestion_artifact,
+                                                                          data_validation_artifact=data_validation_artifact)
         except Exception as e:
             raise CustomException(e, sys) from e    
