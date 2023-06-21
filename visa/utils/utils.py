c0e603dfb8a4bc40ccec29963a43a2628ae44ae2
Yashas PS <53535790+Yashas-pissay@users.noreply.github.com>
2023-06-21 13:49:14 +0530

    Project Completed

--- visa/utils/utils.py
+++ visa/utils/utils.py
@@ -32,8 +32,34 @@ def read_yaml_file(file_path:str)->dict:
             return yaml.safe_load(yaml_file)
     except Exception as e:
         raise CustomException(e,sys) from e
+
+def save_numpy_array_data(file_path: str, array: np.array):
+    """
+    Save numpy array data to file
+    file_path: str location of file to save
+    array: np.array data to save
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, 'wb') as file_obj:
+            np.save(file_obj, array)
+    except Exception as e:
+        raise CustomException(e, sys) from e
     
 
+def save_object(file_path: str, obj):
+    """
+    file_path: str
+    obj: Any sort of object
+    """
+    try:
+        dir_path = os.path.dirname(file_path)
+        os.makedirs(dir_path, exist_ok=True)
+        with open(file_path, "wb") as file_obj:
+            dill.dump(obj, file_obj)
+    except Exception as e:
+        raise CustomException(e, sys) from e
     
 def load_data(file_path: str, schema_file_path: str) -> pd.DataFrame:
     try:
@@ -57,30 +83,25 @@ def load_data(file_path: str, schema_file_path: str) -> pd.DataFrame:
     except Exception as e:
         raise CustomException(e, sys) from e
     
-
-def save_numpy_array_data(file_path: str, array: np.array):
+def load_numpy_array_data(file_path: str) -> np.array:
     """
-    Save numpy array data to file
-    file_path: str location of file to save
-    array: np.array data to save
+    load numpy array data from file
+    file_path: str location of file to load
+    return: np.array data loaded
     """
     try:
-        dir_path = os.path.dirname(file_path)
-        os.makedirs(dir_path, exist_ok=True)
-        with open(file_path, 'wb') as file_obj:
-            np.save(file_obj, array)
+        with open(file_path, 'rb') as file_obj:
+            return np.load(file_obj, allow_pickle=True)
     except Exception as e:
         raise CustomException(e, sys) from e
-
-def save_object(file_path: str, obj):
+    
+def load_object(file_path:str):
     """
     file_path: str
     obj: Any sort of object
     """
     try:
-        dir_path = os.path.dirname(file_path)
-        os.makedirs(dir_path, exist_ok=True)
-        with open(file_path, "wb") as file_obj:
-            dill.dump(obj, file_obj)
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
     except Exception as e:
-        raise CustomException(e, sys) from e        
+        raise CustomException(e, sys) from e
